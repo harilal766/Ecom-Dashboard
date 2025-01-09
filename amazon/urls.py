875786e54b92ed50da9da014cce1eeaa06aef5fa
harilal766 <harilasunil2@gmail.com>
2025-01-09 13:18:07 +0530

    trying bootstrap

--- amazon/urls.py
+++ amazon/urls.py
@@ -8,4 +8,5 @@ app_name = 'amazon'
 urlpatterns = [
     path('amazon',views.amazon_detail_page,name="amazon"),
     path('amazon_reports',views.amazon_shipment_report,name='amazon_reports'),
+    path('add_amazon_store',views.add_amazon_store,name="add_store"),
 ]
