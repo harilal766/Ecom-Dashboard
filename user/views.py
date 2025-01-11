4e14f850812de4c422218313a324c9ea4099977b
harilal766 <harilasunil2@gmail.com>
2025-01-11 13:13:26 +0530

    added register function code

--- user/views.py
+++ user/views.py
@@ -18,20 +18,39 @@ auth_context = {"purpose" : None,
                 }
 
 def register(request):
-    auth_context["purpose"] = "signup"
+    """
+    if the password is strong username and email is not taken already,
+        the code should check if it matches with confirm password
+        if yes, the user can be created
+    """
+    purpose_text = "Sign-Up"
+    auth_context["purpose"] = purpose_text
     auth_context["button_text"] = "Register"
     if request.method == "POST":
-        register_form = Userform(request.POST)
-        if register_form.is_valid():
-            register_form.save()
-            auth_context["form"] = register_form
-            return redirect("home")
+        # request.POST.get("")
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
+
+        if password == confirm_password:
+            user = User.objects.create_user(username=username,password=password)
+            user.save()
+
+            color_text("User Created")
+            return home
+        else:
+            color_text(message="The passwords doesnt match",color="red")
+
+        print(request.POST)
     # credentials : username, password, email, confirm password
     return render(request,"authorization/auth_form.html",auth_context)
 
 def login(request):
-    auth_context["purpose"] = "signin"; auth_context["button_text"] = "Login"
-    return render(request,"authorization/auth_form.html",auth_context)
+    purpose_text = "Sign-In"
+    auth_context["purpose"] = purpose_text; 
+    auth_context["button_text"] = "Login"
+    return render(request,"authorization/auth_form.html",auth_context,)
     # credentials needed to login : username, password
 
 def logout(request):
