5a6b56a52af0ec775b773e827d560344f8ba0b00
harilal766 <harilasunil2@gmail.com>
2025-01-11 17:16:38 +0530

    password strength checker : started

--- static/js/form_validation.js
+++ static/js/form_validation.js
@@ -6,6 +6,23 @@
 submit button should not be visible until the credentials are approved..
 read the password and its length
 */
+
+
+
+
 function passwordStrength() {
-    console.log("pass");
+    const password_field = document.getElementById("password");
+    // check the typed word
+    password_field.addEventListener("keydown",function (event) {
+        let password_input = password_field.value;
+        let title = document.getElementById("title");
+
+        let status = "Weak"; const strength = 8;
+        if (password_input.length >= strength) {
+            status = "Strong";
+        } 
+
+        // update the status on the text box label on each keypress..
+        console.log(status); console.log(title.value);
+    });
 }
