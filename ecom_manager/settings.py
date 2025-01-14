bc0fca3a348f921d3a84297f9786de02582fe9be
harilal766 <harilalsunil2@gmai.com>
2025-01-14 22:19:50 +0530

    removed api keys and added db again

--- ecom_manager/settings.py
+++ ecom_manager/settings.py
@@ -82,10 +82,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'OPTIONS': {
-            'timeout': 20,
-            'key': key,  # Encryption key
-        },
     }
 }
 
