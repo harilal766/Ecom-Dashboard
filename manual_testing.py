2c2e17ae22ee0adbdad8d81f9add6c149efef8c0
harilal766 <harilalsunil2@gmai.com>
2025-01-25 22:37:26 +0530

    lable sort optimization : added seperate conditions for post and amazon, product name based sort done

--- manual_testing.py
+++ manual_testing.py
@@ -91,6 +91,8 @@ from PyPDF2 import PdfReader, PdfWriter
 import re
 from helpers.regex_patterns import post_track_id_pattern,post_order_id_pattern
 
+
+
 def FBA_label_sort(input_pdf_name, input_pdf_path,label_type):
     """
     Deal breakers 
@@ -102,106 +104,75 @@ def FBA_label_sort(input_pdf_name, input_pdf_path,label_type):
         todays_date_folder = f"{from_timestamp(0).split("T")[0]} label split" 
         #todays_date_folder = "25.1.25 lable split"
         todays_folder_path = os.path.join(input_pdf_path, todays_date_folder)
-
         input_pdf_file = os.path.join(input_pdf_path,input_pdf_name)
         """
             open the pdf only if the path exists
             check if the label is amazon or post
         """
-        if os.path.exists(input_pdf_path):
-            with pdfplumber.open(input_pdf_file) as pdf:
-                # make sure the page is not empty
-                
-
-                """   
-                page_count = 0
-                label_summary_dict = {}
-
-                title = None 
-
-                for page in pdf.pages:
-                    page_count += 1
-                    # look out for pages without invoice and shipping label
-                    
-                    page_text = page.extract_text()
-
-                    invoice_pattern = "Invoice Number : IN-6834"
-                    # invoice length : between 4 to 10 
-
-                    page_table = page.extract_table()
-
-
-                    if label_type == "amazon":
-                        if type(page_table) == list:
-                            if len(page_table) > 2: # if this condition succeeds, its not an odd page..
-                                # first row
-                                invoice_page_num = page_count; label_page_number = invoice_page_num -1
-                                heading = page_table[0]; 
-                                # second row
-                                product_details = page_table[1]; 
-                                product_name = (product_details[1].split("|")[0]).replace("\n"," ")
-                                product_qty = product_details[3]
-
-                                if len(page_table) > 5: # mixed items order
-                                    color_text(f"{page_count} - multi item order","red")
-                                    title = "Mixed"
-                                else: # single item order
-                                    title = product_name
-                                    
-                            else: # odd page found.
-                                color_text(f"{label_page_number} odd page detected.","red")
-
-                    # Post label
-                    elif label_type == 'post':
-                        post_order_id = re.findall(post_order_id_pattern,page_text)
-                        if post_order_id:
-                            post_order_id = post_order_id[0]
-
-                        product_details_pattern = r'Ref'
-
-                    if not title == None :
-                        color_text(title)
-                        if title not in label_summary_dict:
-                            label_summary_dict[title] = []
-                            label_summary_dict[title] +=  [label_page_number,invoice_page_num]
-                            print( f"{label_page_number} -> {page_count} : {product_name}, {product_qty} No.s")
-                            color_text("-"*50)
-
-                        
-                if label_summary_dict:
-                    # merge all the pdf files based on product name
-                    for index in range(len(label_summary_dict)):
-
-                        product_name = list(label_summary_dict.keys())[index]
-                        page_nums = list(label_summary_dict.values())[index]
-
-                        # def pdf_merger(out_filename,page_num_list):
-
-                        if len(page_nums) > 0:
-                            reader = PdfReader(input_pdf_file); writer = PdfWriter()
-
-                            new_folder_path = os.path.join(todays_folder_path,input_pdf_name.replace(".pdf","")) # new directory path by replacing the .pdf in the filename
-                            os.makedirs(new_folder_path,exist_ok=True) # creating new directory
-                            out_pdf_path = os.path.join(new_folder_path,product_name) # adding out pdf file to the path
-
-                            for num in page_nums:
-                                writer.add_page(reader.pages[num-1])
-
-
-                            # writing the pages to the file.
-                            with open(f"{out_pdf_path} - {int(len(page_nums)/2)}","wb") as filtered_pdf:
-                                writer.write(filtered_pdf)
-                else:
-                    color_text("Label summary is empty.","red")
-                """
-
-        else:
+        if not os.path.exists(input_pdf_path):
             color_text("The file does not exist..","red")
+        else:
+            if not label_type:
+                color_text("specify the label type, Eg : Amazon, Post etc...","red")
+            else:
+                with pdfplumber.open(input_pdf_file) as pdf:
+                    # make sure the page is not empty
+                    #Varible Initialization
+                    page_number = 0; product_name = None; product_qty = None
+                    label_summary_dict = {}; post_product_line = r"Ref"
+
+                    for page in pdf.pages:
+                        page_number += 1
+
+                        page_text = page.extract_text(); page_table = page.extract_table()
+
+                        if page_text == "": # adding the deal breaker conditions first 
+                            color_text(f"{page_number} : Empty.","red")
+                        else:
+                            if label_type == "amazon":
+                                if len(page_table) <=2:
+                                    color_text("Odd page detected","red")
+                                else:
+                                    #color_text(page_table)
+
+                                    if len(page_table) > 5: # mixed items order
+                                        color_text(f"{page_number} - multi item order","red")
+                                        product_name = "Mixed"
+                                    else: # single item order
+                                        product_row = page_table[1]
+                                        product_name = product_row[1].split("\n")[0].split("|")[0]
+                                        product_qty = product_row[3]
+
+                                        #color_text(product_row[1])
+                                        color_text(f"{page_number} - {product_name} - {product_qty} qty.")
+
+
+
+                            
+                            # label summary making
+                            if product_name and product_qty:
+                                if product_name not in label_summary_dict:
+                                    label_summary_dict[product_name] = []
+                                
+                                label_summary_dict[product_name] += [page_number-1,page_number]
+                                print(label_summary_dict)
+                                    
+                            
+                                    
+
+                                    
+                    print(label_summary_dict)
+
+
+
+                            
 
             
-
     except Exception as e:
-        better_error_handling(e)
+                better_error_handling(e)
+
+
+        
 
 
 
@@ -211,9 +182,10 @@ amazon = dir_switch(win=win_amazon_invoice,lin=lin_amazon_invoice)
 
 post = r"D:\6.SPEED POST"
 
-FBA_label_sort(input_pdf_name="25.1.25 prepaid - 1.pdf", 
-    input_pdf_path = amazon, 
-    label_type = 'amazon')
+lin_post = r"/home/hari/Downloads/"
+
+FBA_label_sort(input_pdf_name="16.10.24 cod.pdf", 
+    input_pdf_path = amazon,label_type='amazon')
 
 
 
