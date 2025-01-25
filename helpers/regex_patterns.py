db4b0e3f3c333ca608a0add6d978244e8490edd5
harilal766 <harilasunil2@gmail.com>
2025-01-25 17:26:19 +0530

    label sorter : need to add more verification conditions to prevent bugs, need to include post label

--- helpers/regex_patterns.py
+++ helpers/regex_patterns.py
@@ -1,7 +1,7 @@
 
 amazon_order_id_pattern = r"\d{3}-\d{7}-\d{7}"
 
-post_order_id_pattern = r'#\d{5}'
+post_order_id_pattern = r'#\d{4,5}'
 
 post_track_id_pattern = r"^E[LZ]\d{9}IN$"
 
--- manual_testing.py
+++ manual_testing.py
@@ -89,108 +89,131 @@ def similarity_count():
 
 from PyPDF2 import PdfReader, PdfWriter
 import re
+from helpers.regex_patterns import post_track_id_pattern,post_order_id_pattern
 
-
-
-def FBA_label_sort(input_pdf_name, input_pdf_path):
+def FBA_label_sort(input_pdf_name, input_pdf_path,label_type):
+    """
+    Deal breakers 
+        Common : page should not be empty
+        Amazon : odd page
+    """
     try:
         # Create a new folder for storing filtered pdf files
-        #todays_date_folder = f"{from_timestamp(0).split("T")[0]} label split" 
-        todays_date_folder = "25.1.25 lable split"
+        todays_date_folder = f"{from_timestamp(0).split("T")[0]} label split" 
+        #todays_date_folder = "25.1.25 lable split"
         todays_folder_path = os.path.join(input_pdf_path, todays_date_folder)
 
-        pdf_file = os.path.join(input_pdf_path,input_pdf_name)
-
-        with pdfplumber.open(pdf_file) as pdf:
-            page_count = 0
-            label_summary_dict = {}
-            for page in pdf.pages:
-                page_count += 1
-                # look out for pages without invoice and shipping label
+        input_pdf_file = os.path.join(input_pdf_path,input_pdf_name)
+        """
+            open the pdf only if the path exists
+            check if the label is amazon or post
+        """
+        if os.path.exists(input_pdf_path):
+            with pdfplumber.open(input_pdf_file) as pdf:
+                # make sure the page is not empty
                 
-                other_details = page.extract_text()
 
-                invoice_pattern = "Invoice Number : IN-6834"
-                # invoice length : between 4 to 10 
+                """   
+                page_count = 0
+                label_summary_dict = {}
 
-                table = page.extract_table()
-                if type(table) == list:
-                    """
-                    the table inside the invoice page is returned as a linked list
-                        1. titles, 2. product name and other values, last two lists are the total and amount in words.
-                    """
-                    if len(table) > 2: # if this condition succeeds, its not an odd page..
-                        # first row
-                        invoice_page_num = page_count; label_page_number = invoice_page_num -1
-                        heading = table[0]; 
-                        # second row
-                        product_details = table[1]; 
-                        product_name = (product_details[1].split("|")[0]).replace("\n"," ")
-                        product_qty = product_details[3]
+                title = None 
+
+                for page in pdf.pages:
+                    page_count += 1
+                    # look out for pages without invoice and shipping label
+                    
+                    page_text = page.extract_text()
+
+                    invoice_pattern = "Invoice Number : IN-6834"
+                    # invoice length : between 4 to 10 
+
+                    page_table = page.extract_table()
 
 
-                        # final row
-                        amount_in_words = table[-2]; signature = table[-1]
+                    if label_type == "amazon":
+                        if type(page_table) == list:
+                            if len(page_table) > 2: # if this condition succeeds, its not an odd page..
+                                # first row
+                                invoice_page_num = page_count; label_page_number = invoice_page_num -1
+                                heading = page_table[0]; 
+                                # second row
+                                product_details = page_table[1]; 
+                                product_name = (product_details[1].split("|")[0]).replace("\n"," ")
+                                product_qty = product_details[3]
 
-                        if len(table) > 5: # mixed items order
-                            color_text(f"{page_count} - multi item order","red")
-                            label_summary_dict["Mixed"] = []
-                            label_summary_dict["Mixed"] +=  [label_page_number,invoice_page_num]
+                                if len(page_table) > 5: # mixed items order
+                                    color_text(f"{page_count} - multi item order","red")
+                                    title = "Mixed"
+                                else: # single item order
+                                    title = product_name
+                                    
+                            else: # odd page found.
+                                color_text(f"{label_page_number} odd page detected.","red")
 
-                        else: # single item order
-                            if product_name not in label_summary_dict:
-                                label_summary_dict[product_name] = []
+                    # Post label
+                    elif label_type == 'post':
+                        post_order_id = re.findall(post_order_id_pattern,page_text)
+                        if post_order_id:
+                            post_order_id = post_order_id[0]
 
-                            label_summary_dict[product_name] += [label_page_number,invoice_page_num]
+                        product_details_pattern = r'Ref'
 
+                    if not title == None :
+                        color_text(title)
+                        if title not in label_summary_dict:
+                            label_summary_dict[title] = []
+                            label_summary_dict[title] +=  [label_page_number,invoice_page_num]
                             print( f"{label_page_number} -> {page_count} : {product_name}, {product_qty} No.s")
                             color_text("-"*50)
-                    else: # odd page found.
-                        color_text(f"{label_page_number} odd page detected.","red")
 
                         
+                if label_summary_dict:
+                    # merge all the pdf files based on product name
+                    for index in range(len(label_summary_dict)):
+
+                        product_name = list(label_summary_dict.keys())[index]
+                        page_nums = list(label_summary_dict.values())[index]
+
+                        # def pdf_merger(out_filename,page_num_list):
+
+                        if len(page_nums) > 0:
+                            reader = PdfReader(input_pdf_file); writer = PdfWriter()
+
+                            new_folder_path = os.path.join(todays_folder_path,input_pdf_name.replace(".pdf","")) # new directory path by replacing the .pdf in the filename
+                            os.makedirs(new_folder_path,exist_ok=True) # creating new directory
+                            out_pdf_path = os.path.join(new_folder_path,product_name) # adding out pdf file to the path
+
+                            for num in page_nums:
+                                writer.add_page(reader.pages[num-1])
+
+
+                            # writing the pages to the file.
+                            with open(f"{out_pdf_path} - {int(len(page_nums)/2)}","wb") as filtered_pdf:
+                                writer.write(filtered_pdf)
+                else:
+                    color_text("Label summary is empty.","red")
+                """
+
+        else:
+            color_text("The file does not exist..","red")
+
             
-            # merge all the pdf files based on product name
-            for index in range(len(label_summary_dict)):
-
-                product_name = list(label_summary_dict.keys())[index]
-                page_nums = list(label_summary_dict.values())[index]
-
-                # def pdf_merger(out_filename,page_num_list):
-
-                if len(page_nums) > 0:
-                    reader = PdfReader(pdf_file); writer = PdfWriter()
-
-                    for num in page_nums:
-                        writer.add_page(reader.pages[num-1])
-
-
-                    new_folder_path = os.path.join(todays_folder_path,input_pdf_name.replace(".pdf","")) # new directory path by replacing the .pdf in the filename
-                    os.makedirs(new_folder_path,exist_ok=True) # creating new directory
-                    out_pdf_path = os.path.join(new_folder_path,product_name) # adding out pdf file to the path
-
-                    # writing the pages to the file.
-                    with open(f"{out_pdf_path} - {int(len(page_nums)/2)}","wb") as filtered_pdf:
-                        writer.write(filtered_pdf)
-
-            print(label_summary_dict)
 
     except Exception as e:
         better_error_handling(e)
 
 
-"""
+
 # both of these should be from front end in django
-input_pdf_name = "25.1.25 cod-4.pdf"
-input_pdf_path = dir_switch(win=win_amazon_invoice,lin=lin_amazon_invoice)
-
-FBA_label_sort(input_pdf_name=input_pdf_name, input_pdf_path=input_pdf_path)
-
-"""
+amazon = dir_switch(win=win_amazon_invoice,lin=lin_amazon_invoice)
 
 
+post = r"D:\6.SPEED POST"
 
-
+FBA_label_sort(input_pdf_name="25.1.25 prepaid - 1.pdf", 
+    input_pdf_path = amazon, 
+    label_type = 'amazon')
 
 
 
@@ -207,25 +230,12 @@ FBA_label_sort(input_pdf_name=input_pdf_name, input_pdf_path=input_pdf_path)
 
 
 
-def pppp():
-    from helpers.regex_patterns import post_track_id_pattern,post_order_id_pattern
-
-    pdf_route = r"D:\6.SPEED POST\4 split.pdf"
-
-    with pdfplumber.open(pdf_route) as pdf:
-        co = 0 
-        for page in pdf.pages:
-
-            text = page.extract_text_simple()
-
-            id = page.extract_text_simple()
-
-            id_match = re.match(post_order_id_pattern,text)
-            if id_match:
-                print(id_match[0],sep=",")
 
 
 
 
-pppp()
+
+
+
+
 
