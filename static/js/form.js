57bb802e51025e9e253c7781d062a6d04154d826
harilal766 <harilalsunil2@gmai.com>
2025-02-09 21:41:19 +0530

    store form : addes status if name exists or not

--- static/js/form.js
+++ static/js/form.js
@@ -1,34 +1,39 @@
 async function nameValidation(inputNameField,endpoint,key,statusId){
-    let status = "short";
-    let existingNames = []; let statusBar = document.getElementById(statusId);
+    let existingNames = []
+    // name status
+    let statusBar = document.getElementById(statusId);
+    statusBar.style.color = "red"; let status = "";
+
     try{ 
         let response = await fetch(endpoint);
-        // find existing names 
-        if (response){
-            let data = await response.json();
-            
-            data.forEach(store => {
-                existingNames.push(store[key]);
-            });
-
-            console.log(existingNames);
-
-            inputNameField.addEventListener("blur",event =>{
-                let inputValue = inputNameField.value;
-
-                if (inputValue > 5 ){
-                    status = "Strong";
-                }
-                //!existingNames.includes(inputName);
-                statusBar.innerText = `Input : ${inputValue} - status ${status}`;
-            });
-
-            
-        }
-        else {
-            console.log("Empty response");
-        }
+        let stores = await response.json();
         
+        stores.forEach(store => {
+            existingNames.push(store[key]);
+        });
+        console.log(existingNames);
+
+        const StoreName = document.getElementById("storeName");
+
+        StoreName.addEventListener("input",function(event){
+            const typedName = event.target.value;
+            // display status only if input value is present
+            if (inputNameField.value) {
+                if (existingNames.includes(typedName)){
+                    statusBar.style.color = "green"
+                    status = "Name already exists."
+                }
+                else {
+                    statusBar.style.color = "green"
+                    status = "Name available";
+                }
+                statusBar.innerText = status;
+                inputNameField.focus();
+            }
+            else{
+                statusBar.innerText = "";
+            }            
+        });
     }
     catch (error){
         console.log("Error",error);
@@ -36,11 +41,20 @@ async function nameValidation(inputNameField,endpoint,key,statusId){
 }
 
 
+function storeFormBuilder (type){
+    console.log(type);
+    storeType.addEventListener("change",function(event){
+        const selectedType = event.target.value;
+        console.log(selectedType);
+    });
+}
+
 let storeNameField = document.getElementById("storeName");
+const storeType = document.getElementById("storePlatform").value;
+
 
 nameValidation(inputNameField = storeNameField,
     endpoint = "http://127.0.0.1:8000/api/stores/",key = "store_name", statusId = "statusBar"); 
 
+storeFormBuilder(type = storeType);
 
-const storePlatform = document.getElementById("storePlatform");
-console.log(999);
