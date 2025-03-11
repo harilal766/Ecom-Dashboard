3970ddae40b9854a2e199980b97edc08c10f59dd
harilal766 <harilasunil2@gmail.com>
2025-03-11 16:12:23 +0530

    updated requirements.txt

--- ecom_manager/urls.py
+++ ecom_manager/urls.py
@@ -21,7 +21,6 @@ from drf.urls import router
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include(router.urls)),
