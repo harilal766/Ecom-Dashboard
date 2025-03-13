47ec758e734b1dd51215c873bd7c8a6bf0debb4c
harilal766 <harilasunil2@gmail.com>
2025-03-13 16:34:19 +0530

    added hightlighting to the selected store title

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -11,14 +11,24 @@ const addedStoresTitles = addedStoresDiv.querySelectorAll(".store-title");
 const dashboardTitle = document.getElementById("dashboardTitle");
 
 // click the title and return the store name from it
-let selectedTitle = addedStoresTitles[0];
-selectedTitle.classList.add("active");
+let defaultSelection = addedStoresTitles[0];
+console.log(`Default store : ${defaultSelection.innerHTML}`);
+defaultSelection.classList.add("active");
 
-addedStoresTitles.forEach((title) => {
-    title.addEventListener("click",function(event){
-        selectedTitle.style.color = "";
-        console.log(title);
-        selectedTitle = event.target;
-        selectedTitle.classList.add("active");
+addedStoresTitles.forEach((title) =>{
+    console.log(title);
+
+    title.addEventListener("click", function(event){
+        // removing the highlighting from the preselected title
+        addedStoresTitles.forEach((preSelection) => preSelection.classList.remove("active"));
+        
+        event.target.classList.add("active");
     });
+    
 });
+
+
+/*
+access the store debrief api
+and print its contents
+*/ 
